fe83305da41d6ad0b5b73dcf10ee175a6da33ef8
Konstantin <tenenbaum1994@gmail.com>
2025-09-20 16:01:33 +0700

    Реализован FilmServiceImpl

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
@@ -1,42 +1,54 @@
 package ru.yandex.practicum.filmorate.service.film;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.util.List;
 
+@Service
 public class FilmServiceImpl implements FilmService {
+
+    private final FilmStorage filmStorage;
+
+    @Autowired
+    public FilmServiceImpl(FilmStorage filmStorage) {
+        this.filmStorage = filmStorage;
+    }
+
     @Override
     public Film addFilm(Film film) {
-        return null;
+        return filmStorage.addFilm(film);
     }
 
     @Override
     public Film getFilm(int id) {
-        return null;
+        return filmStorage.getFilm(id);
     }
 
     @Override
     public List<Film> getFilms() {
-        return List.of();
+        return filmStorage.getFilms();
     }
 
     @Override
     public Film updateFilm(Film film) {
-        return null;
+        return filmStorage.updateFilm(film);
     }
 
     @Override
     public void addLike(int filmId, int userId) {
-
+        filmStorage.addLike(filmId, userId);
     }
 
     @Override
     public void removeLike(int filmId, int userId) {
-
+        filmStorage.removeLike(filmId, userId);
     }
 
     @Override
     public List<Film> getPopularFilms(int count) {
-        return List.of();
+        return filmStorage.getPopularFilms(count);
     }
 }
