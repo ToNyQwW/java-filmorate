806f6a52e6d5a3e2969e0dea69793f162c83afe8
Konstantin <tenenbaum1994@gmail.com>
2025-09-21 16:26:19 +0700

    доработки в ExceptionHandlerAdvice

--- src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionHandlerAdvice.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionHandlerAdvice.java
@@ -14,12 +14,12 @@ import java.util.Map;
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleNotFoundException(final NotFoundException e) {
+    public ResponseEntity<Map<String, String>> handleNotFoundException(NotFoundException e) {
         return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({ConstraintViolationException.class, MethodArgumentNotValidException.class})
-    public ResponseEntity<Map<String, String>> handleValidationException(final Exception e) {
+    public ResponseEntity<Map<String, String>> handleValidationException(Exception e) {
         return new ResponseEntity<>(Map.of("message", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
