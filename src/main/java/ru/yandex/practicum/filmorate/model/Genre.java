6d0617842c52c835c796062590558f8979e8e778
Konstantin <tenenbaum1994@gmail.com>
2025-10-13 21:44:12 +0700

    добавлена сущность Genre

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(of = "id")
+public class Genre {
+
+    private int id;
+
+    @NotBlank
+    private String name;
+}
