3fb98d1bbf17603ada79e0e08a3fb8e94baf3529
Konstantin <tenenbaum1994@gmail.com>
2025-10-14 12:51:08 +0700

    реализован GenreDao

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/Genre/GenreDao.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.filmorate.dao.Genre;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.entity.Genre;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class GenreDao {
+
+    private static final String GET_GENRE_SQL = """
+                    SELECT genre_id as id,
+                           name
+                    FROM genre
+            """;
+
+    private static final String GET_GENRE_BY_ID_SQL = """
+                    SELECT genre_id as id,
+                           name
+                    FROM genre
+                    WHERE genre_id = ?
+            """;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<Genre> getAllGenres() {
+        return jdbcTemplate.query(GET_GENRE_SQL, new BeanPropertyRowMapper<>(Genre.class));
+    }
+
+    public Genre getGenreById(Long id) {
+        return jdbcTemplate.queryForObject(GET_GENRE_BY_ID_SQL, new BeanPropertyRowMapper<>(Genre.class), id);
+    }
+}
