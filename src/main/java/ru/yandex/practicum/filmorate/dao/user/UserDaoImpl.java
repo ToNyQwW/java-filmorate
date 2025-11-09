4b262a5d5a25516caff9b907279c20dc3ba48988
Konstantin <tenenbaum1994@gmail.com>
2025-11-09 17:03:26 +0700

    фикс кодСтайл2

--- src/main/java/ru/yandex/practicum/filmorate/dao/user/UserDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/user/UserDaoImpl.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.dao.friendship.FriendshipDao;
 import ru.yandex.practicum.filmorate.dao.user.sql.*;
 import ru.yandex.practicum.filmorate.entity.User;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
