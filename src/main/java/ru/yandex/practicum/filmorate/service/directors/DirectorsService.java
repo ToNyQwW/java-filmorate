b94396a07e74a56e624246121696e7d418533299
Konstantin <tenenbaum1994@gmail.com>
2025-11-09 17:05:23 +0700

    фикс кодСтайл3

--- src/main/java/ru/yandex/practicum/filmorate/service/directors/DirectorsService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/directors/DirectorsService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service.directors;
 import ru.yandex.practicum.filmorate.entity.Director;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface DirectorsService {
 
